6524fec35b14ba578684d6912d84fee2106a4f8b
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2022-04-24 04:56:52 +0200

    Display results

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,11 +4,54 @@ function Game() {
     return (
       <div id="gameContainer">
         <h1>Please choose one of the following:</h1>
-        <button class="myButton" id="rock">Rock ✊</button>
-        <button class="myButton" id="paper">Paper ✋</button>
-        <button class="myButton" id="scissors">Scissors ✌️</button>
+        <button className="myButton" id="rock" onClick={(e) => displayResults(e)}>Rock ✊</button>
+        <button className="myButton" id="paper" onClick={(e) => displayResults(e)}>Paper ✋</button>
+        <button className="myButton" id="scissors" onClick={(e) => displayResults(e)}>Scissors ✌️</button>
+        <div id="results" />
       </div>
     );
+  }
+
+  function generateComputerChoice() {
+    const choices = [
+      {key: 'rock', value: 'Rock ✊'}, 
+      {key: 'paper', value: 'Paper ✋'}, 
+      {key: 'scissors', value: 'Scissors ✌️'}
+    ];
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+  }
+
+  function getWinner(userChoice, computerChoice) {
+    switch (userChoice + computerChoice) {
+      case 'rockscissors':
+      case 'paperrock':
+      case 'scissorspaper':
+        return 'You win!';
+      case 'rockpaper':
+      case 'paperscissors':
+      case 'scissorsrock':
+        return 'Computer wins!';
+      default:
+        return "It's a draw!";
+    }
+  }
+  
+  function displayResults(e) {
+    let userChoice = e.target;
+    let computerChoice = generateComputerChoice();
+
+    const userChoiceDisplay = document.createElement('h1');
+    userChoiceDisplay.innerHTML = 'Your choice: ' + userChoice.innerHTML;
+
+    const computerChoiceDisplay = document.createElement('h1');
+    computerChoiceDisplay.innerHTML = "Computer's choice: " + computerChoice.value;
+
+    const winnerDisplay = document.createElement('h1');
+    winnerDisplay.innerHTML = getWinner(userChoice.id, computerChoice.key);
+
+    document.getElementById('results').innerHTML = "";
+    document.getElementById('results').append(userChoiceDisplay, computerChoiceDisplay, winnerDisplay);
   }
   
   export default Game;

5981a5bfcc2eb760d6a01120b2e59afdc0ee01b5
adamkarmander <71024067+adamkarmander@users.noreply.github.com>
2022-04-24 04:13:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Score from './components/Score';
+
+function App() {
+  return (
+    <div id="App">
+      <Score />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Score.js
@@ -0,0 +1,11 @@
+import '../Score.css';
+
+function Score() {
+    return (
+      <div>
+          <h1>Score: 0-0 (You-Computer)</h1>
+      </div>
+    );
+  }
+  
+  export default Score;
